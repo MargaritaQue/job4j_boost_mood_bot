c9f005840c35932febde6ea1b862fc208895fc48
margarita <margarita.que@mail.ru>
2025-03-27 22:18:56 +0400

    Жизненный цикл Spring Bean [#505292]

--- src/main/java/ru/job4j/AchievementService.java
+++ src/main/java/ru/job4j/AchievementService.java
@@ -1,4 +1,17 @@
 package ru.job4j;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class AchievementService {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean is going through init.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean will be destroyed now.");
+    }
 }
