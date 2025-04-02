d86ea8ac7236bca7e08adcb6c60b73313ce0235a
margarita <margarita.que@mail.ru>
2025-04-02 14:39:35 +0400

    Механизм рассылки уведомлений [#505297]

--- src/main/java/ru/job4j/bmb/service/TgRemoteService.java
+++ src/main/java/ru/job4j/bmb/service/TgRemoteService.java
@@ -66,7 +66,7 @@ public class TgRemoteService extends TelegramLongPollingBot {
                 user.setClientId(message.getFrom().getId());
                 user.setChatId(chatId);
                 userRepository.save(user);
-                send(sendButtons(chatId));  // Метод для отправки кнопок пользователю
+                send(sendButtons(chatId));
             }
         }
     }
