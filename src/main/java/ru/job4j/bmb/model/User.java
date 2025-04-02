154438764ad400277479d6780962ee85779ed5c0
margarita <margarita.que@mail.ru>
2025-04-02 11:32:09 +0400

    @Profile | Пользователи системы [#505303]

---
+++ src/main/java/ru/job4j/bmb/model/User.java
@@ -0,0 +1,57 @@
+package ru.job4j.bmb.model;
+
+import java.util.Objects;
+
+public class User {
+
+    private Long id;
+    private long clientId;
+    private long chatId;
+
+    public User(long chatId, long clientId, Long id) {
+        this.chatId = chatId;
+        this.clientId = clientId;
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public long getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(long clientId) {
+        this.clientId = clientId;
+    }
+
+    public long getChatId() {
+        return chatId;
+    }
+
+    public void setChatId(long chatId) {
+        this.chatId = chatId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        User user = (User) o;
+        return clientId == user.clientId && chatId == user.chatId && Objects.equals(id, user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, clientId, chatId);
+    }
+}
